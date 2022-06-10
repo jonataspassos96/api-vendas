c1e8f92407adc2e6ffba311962316b8119a63ee7
Jonatas Passos <jpc080496@gmail.com>
2022-06-10 11:15:28 -0300

    feat: create server

---
+++ src/shared/http/server.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata';
+import express, {
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from 'express';
+import cors from 'cors';
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (
+    err: ErrorRequestHandler,
+    req: Request,
+    res: Response,
+    _next: NextFunction,
+  ) => {
+    if (err instanceof AppError) {
+      return res.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    return res.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  },
+);
+
+app.listen(3333, () => console.log('Server started on port 3333'));
