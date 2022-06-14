98edfdc6b30cdf141daa62cfba5206e07e9503a2
Jonatas Passos <jpc080496@gmail.com>
2022-06-14 11:55:09 -0300

    refactor: add celebrate errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
 import express, {
-  ErrorRequestHandler,
   NextFunction,
   Request,
   Response,
 } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
 import '@shared/typeorm';
@@ -17,25 +18,20 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use(
-  (
-    err: ErrorRequestHandler,
-    _req: Request,
-    res: Response,
-    _next: NextFunction,
-  ) => {
-    if (err instanceof AppError) {
-      return res.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
+app.use(errors());
 
-    return res.status(500).json({
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
       status: 'error',
-      message: 'internal server error',
+      message: err.message,
     });
-  },
-);
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'internal server error',
+  });
+});
 
 app.listen(3333, () => console.log('Server started on port 3333'));
