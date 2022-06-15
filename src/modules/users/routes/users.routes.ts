d950a52aad7d789b85f3cbe50ee64ae0f0ff873e
Jonatas Passos <jpc080496@gmail.com>
2022-06-14 22:04:22 -0300

    feat: implemented user route

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,52 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import UsersController from '../controllers/UsersController';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+
+usersRouter.route('/')
+  .get(usersController.list)
+  .post(
+    celebrate({
+      [Segments.BODY]: {
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+      },
+    }),
+    usersController.create,
+  );
+
+usersRouter.route('/:id')
+  .get(
+    celebrate({
+      [Segments.PARAMS]: {
+        id: Joi.string().uuid().required(),
+      },
+    }),
+    usersController.show,
+  )
+  .put(
+    celebrate({
+      [Segments.PARAMS]: {
+        id: Joi.string().uuid().required(),
+      },
+      [Segments.BODY]: {
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+      },
+    }),
+    usersController.update,
+  )
+  .delete(
+    celebrate({
+      [Segments.PARAMS]: {
+        id: Joi.string().uuid().required(),
+      },
+    }),
+    usersController.delete,
+  );
+
+export default usersRouter;
