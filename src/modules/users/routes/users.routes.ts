a49666cc90f538dd41a957d607c8428dc86613f0
Jonatas Passos <jpc080496@gmail.com>
2022-06-16 18:08:32 -0300

    feat: implemented updateAvatar routes

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
 import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRouter.route('/')
   .get(
@@ -54,6 +60,13 @@ usersRouter.route('/:id')
       },
     }),
     usersController.delete,
+  );
+
+usersRouter.route('/avatar')
+  .patch(
+    isAuthenticated,
+    upload.single('avatar'),
+    usersAvatarController.update,
   );
 
 export default usersRouter;
