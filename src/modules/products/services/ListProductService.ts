fad3c5f27f131f0b9c69bed5138e722bb4e4b66c
Jonatas Passos <jpc080496@gmail.com>
2022-07-02 10:23:42 -0300

    refactor: add redisCache to ListProductService

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,3 +1,4 @@
+import RedisCache from '@shared/cache/RedisCache';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepositoriy';
@@ -6,7 +7,15 @@ class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepositoriy = getCustomRepository(ProductRepository);
 
-    const products = await productsRepositoriy.find();
+    const redisCache = new RedisCache();
+
+    let products = await redisCache.recover<Product[]>('api-vendas-PRODUCT_LIST');
+
+    if (!products) {
+      products = await productsRepositoriy.find();
+
+      await redisCache.save('api-vendas-PRODUCT_LIST', products);
+    }
 
     return products;
   }
